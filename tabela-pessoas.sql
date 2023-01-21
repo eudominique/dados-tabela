a692a21e359ec12b70946f8436460b36758df24e
eudominique <opsdominique@gmail.com>
2023-01-21 10:13:26 -0300

    primeiro commit

---
+++ tabela-pessoas.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL PRIMARY KEY
+    nome VARCHAR[] NOT NULL
+    nascimento DATE
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Clara', '1996-05-21')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Augusto', '1990-07-12')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Bianca', '1999-10-04')
